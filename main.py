f196f0f68f30042269bd8c703d8753cc4cf9afca
hallma886 <hallma886@northwested.org>
2024-11-06 12:47:11 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+# Matthew hall
+# 1 November 2024
+# FOR Loops
+
+# Task 4-3
+#for n in range(1,21):
+#    print(n)
+# print()
+# Task 4-4
+# for n in range(1,1000001):
+#    print(n)
+# print()
+# Task 4-5
+# my_numbers = []
+# for n in range(1,1_000_001):
+#    print(f'The number Python generated was: {n}')
+#    my_numbers.append(n)
+# sum_of_my_numbers = sum(my_numbers)
+# print(sum_of_my_numbers)
+# print()
+# Task 4-6
+# for n in range(1,21,2):
+#    print(n)
+# print()
+# Task 4-7
+# for n in range(3,31,3):
+#    print(n)
+# print()
+# Task 4-8
+# for n in range(1,11):
+#    print(n**3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
